ab6140b695f84e84ed995db87ac3b429dca88dd1
anders-wartoft <anders.wartoft@gmail.com>
2024-05-03 07:22:30 +0200

    test

---
+++ src/test/gap_detector_test.go
@@ -0,0 +1,107 @@
+package test
+
+import (
+	"testing"
+
+	"sitia.nu/transfer/gap_util"
+)
+
+func TestAll(t *testing.T) {
+    // Test case 1: valid id
+    key := "topic_partition"
+	number := int64(1); 
+	doTest(t, key, number, false)
+
+	// Test with next number
+	number = int64(2)
+	doTest(t, key, number, false)
+
+	// Test with a gap
+	number = int64(4)
+	doTest(t, key, number, false)
+
+	// Check that we have one valid gap
+	gaps := gap_util.GetAllGaps(key)
+	if (len(gaps.Gaps) != 1) {
+		t.Errorf("Wrong number of gaps. Expected %d but got %d", 1, len(gaps.Gaps))
+	}
+
+	// Check the From and To values in the gap
+	if (gaps.Gaps[0].From != 3) {
+		t.Errorf("Wrong From in the gap. Expected %d but got %d", 
+			3, gaps.Gaps[0].From)
+	}
+	if (gaps.Gaps[0].To != 3) {
+		t.Errorf("Wrong To in the gap. Expected %d but got %d", 
+			3, gaps.Gaps[0].To)
+	}
+
+	// Now, send number 3 and check that we have no gaps
+	doTest(t, key, int64(3), false)
+	// GetAllGaps return a copy, so run that again
+	gaps = gap_util.GetAllGaps(key)
+	if (len(gaps.Gaps) != 0) {
+		t.Errorf("Wrong number of gaps. Expected %d but got %d", 1, len(gaps.Gaps))
+	}
+
+//-------------------------------------
+	// Test with a larger gap
+	number = int64(10)
+	doTest(t, key, number, false)
+
+	// Check that we have one valid gap
+	gaps = gap_util.GetAllGaps(key)
+	if (len(gaps.Gaps) != 1) {
+		t.Errorf("Wrong number of gaps. Expected %d but got %d", 1, len(gaps.Gaps))
+	}
+
+	// Check the From and To values in the gap
+	if (gaps.Gaps[0].From != 5) {
+		t.Errorf("Wrong From in the gap. Expected %d but got %d", 
+			5, gaps.Gaps[0].From)
+	}
+	if (gaps.Gaps[0].To != 9) {
+		t.Errorf("Wrong To in the gap. Expected %d but got %d", 
+			9, gaps.Gaps[0].To)
+	}
+
+	// Now, send number 7 and check that we have two gaps
+	doTest(t, key, int64(7), false)
+	// GetAllGaps return a copy, so run that again
+	gaps = gap_util.GetAllGaps(key)
+	if (len(gaps.Gaps) != 2) {
+		t.Errorf("Wrong number of gaps. Expected %d but got %d", 2, len(gaps.Gaps))
+	}
+	// Check the From and To values in the gap
+	if (gaps.Gaps[0].From != 5) {
+		t.Errorf("Wrong From in the gap. Expected %d but got %d", 
+			5, gaps.Gaps[0].From)
+	}
+	if (gaps.Gaps[0].To != 6) {
+		t.Errorf("Wrong To in the gap. Expected %d but got %d", 
+			6, gaps.Gaps[0].To)
+	}
+
+	// Check the From and To values in the gap
+	if (gaps.Gaps[1].From != 8) {
+		t.Errorf("Wrong From in the gap. Expected %d but got %d", 
+			8, gaps.Gaps[0].From)
+	}
+	if (gaps.Gaps[1].To != 9) {
+		t.Errorf("Wrong To in the gap. Expected %d but got %d", 
+			9, gaps.Gaps[0].To)
+	}
+	
+
+
+
+	// And test with a duplicate
+	doTest(t, key, number, true)
+}
+
+func doTest (t *testing.T, key string, number int64, expected bool) {
+	check := gap_util.CheckNextNumber(key, number)
+	if check != expected {
+		t.Errorf("Expected CheckNextNumber returning %t, expected %t", check, expected)
+	}
+}
---
+++ src/test/udp_test.go
@@ -0,0 +1,121 @@
+package test
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+
+	"sitia.nu/transfer/protocol"
+	"sitia.nu/transfer/udp"
+)
+
+func TestSendMessage(t *testing.T) {
+    tests := []struct {
+        name     string
+        mtu      uint16
+        id       string
+        message  string
+        expected int
+    }{
+        {
+            name:     "Test 1",
+            mtu:      100,
+            id:       "testID",
+            message:  "This is a test message",
+            expected: 1,
+        },
+        {
+            name:     "Test 2",
+            mtu:      25,
+            id:       "testID",
+            message:  "This is a longer test message that should be split into multiple parts. abcdefghijklmnopqrstuvwxyzåäö",
+            expected: 18,
+        },
+        // add more test cases as needed
+    }
+
+    for _, tt := range tests {
+        t.Run(tt.name, func(t *testing.T) {
+            actual := protocol.FormatMessage(protocol.TYPE_MESSAGE, tt.id, []byte(tt.message), uint16(tt.mtu))
+            // check the result
+            if !reflect.DeepEqual(len(actual), tt.expected) {
+                t.Errorf("FormatMessage. Expected %d messages, got %d", tt.expected, len(actual))
+            }
+        })
+    }
+}
+
+// package public
+var cache = protocol.CreateMessageCache()
+
+
+
+func handleMessage(received []byte) {
+    fmt.Printf("*Received %s\n", received)
+    _, _, message, ok := protocol.ParseMessage(received, cache)
+    if ok == nil {
+        fmt.Printf("Message: %s\n", message)
+    }
+}
+
+func TestReceiverMessage(t *testing.T) {
+    address := "127.0.0.1"
+    port := 1234
+    mtuLength := uint16(1500)
+
+    udp.ListenUDP(address, port, handleMessage, mtuLength)
+}
+
+
+func TestSendMessageEncrypted(t *testing.T) {
+    tests := []struct {
+        name     string
+        mtu      uint16
+        id       string
+        message  string
+        expected uint16
+    }{
+        {
+            name:     "Test 1",
+            mtu:      100,
+            id:       "testID",
+            message:  "This is a test message",
+            expected: 1,
+        },
+        {
+            name:     "Test 2",
+            mtu:      25,
+            id:       "testID",
+            message:  "This is a longer test message that should be split into multiple parts. abcdefghijklmnopqrstuvwxyzåäö",
+            expected: 25,
+        },
+        // add more test cases as needed
+    }
+    // func FormatMessage(id string, message string, mtu int) []string {
+    cache := protocol.CreateMessageCache()
+    key := []byte("your-32-byte-key-here!abcdefghij") // replace with your 32-byte key
+    
+    for _, tt := range tests {
+        t.Run(tt.name, func(t *testing.T) {
+            ciphertext, err := protocol.Encrypt([]byte(tt.message), key)
+            if err != nil {
+                fmt.Println(err)
+                t.Fail()
+            }
+            actual := protocol.FormatMessage(protocol.TYPE_MESSAGE, tt.id, ciphertext, tt.mtu)
+            for _, str := range actual {
+                messageType, _, encrypted, _ := protocol.ParseMessage(str, cache) // Pass the dereferenced value of cache
+                if messageType == protocol.TYPE_MESSAGE {
+                    // Complete message
+                    decrypted, _ := protocol.Decrypt(encrypted, key)
+                    fmt.Printf("decrypted: '%s'\n", string(decrypted))
+                    fmt.Printf("expected:  '%s'\n", string(tt.message));
+                    // check the result
+                    if !reflect.DeepEqual(string(decrypted), tt.message) {
+                        t.Errorf("FormatMessage(%d, %s, %s) = %v; expected %v", tt.mtu, tt.id, tt.message, actual, tt.expected)
+                    }
+                } // else the message is empty (part of a larger message)
+            }
+        })
+    }
+}
